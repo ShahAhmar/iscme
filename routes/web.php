1facaebf74bc0c563574ea65ea75db0610c8ea5e
Shah Ahmar <shahahmar882@gmail.com>
2026-07-14 08:31:39 +0500

    Add static submission page for CMT Acknowledgment

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,11 @@ Route::prefix('admin')->group(function () {
     Route::post('pages/{page}/builder', [PageController::class, 'saveBuilder'])->name('admin.pages.builder.save');
 });
 
+// CMT Submission Acknowledgment Page
+Route::get('/submission', function () {
+    return 'CMT ACKNOWLEDGMENT
+The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.';
+});
+
 // Dynamic Page Routing (Must be at the bottom)
 Route::get('/{slug?}', [FrontendController::class, 'show'])->name('page.show');
